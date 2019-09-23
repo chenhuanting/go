6f450154faf29861516af892bbc020aff07bb46f
cht <2272650876@qq.com>
2019-09-23 12:46:50 +0800

    addtion temp value

--- recieveImage/main.go
+++ recieveImage/main.go
@@ -7,7 +7,59 @@ import (
 	"io/ioutil"
 	"time"
 	"encoding/base64"
+	"encoding/json"
 )
+type ResponseStruct struct{
+	Code int `json:"code"`
+	Message string `json:"message"`
+	Data interface{} `json:"data"`
+} 
+type ResponseData struct{
+	Value string `json:"value"`
+}
+var tempValue string;
+
+func saveTempValue(temp string){
+	tempValue = temp;
+}
+func getTempValue() string {
+
+	return tempValue
+}
+
+func updateTempValue(w http.ResponseWriter,r *http.Request){
+	r.ParseForm()
+	fmt.Println("path",r.URL.Path)
+	if r.Method == "GET"{
+		var sendValue string 
+		sendValue = getTempValue()
+		fmt.Println("temp control value ：",sendValue)
+		d := ResponseData{
+			Value: sendValue,
+		}
+		s := ResponseStruct{
+			Code : 200,
+			Message : "recieve sucess ",
+			Data : d,
+		}
+		b , _ := json.Marshal(s)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(b)
+
+	}else{
+		//fmt.Println("Heard:",r.Header)
+		//fmt.Println("Body:",r.Body)
+		tempValueBuff := r.Form["temp"]
+
+		tempValue = string(tempValueBuff[0])
+		fmt.Println("temp value:",tempValue)
+		saveTempValue(tempValue)
+		
+		fmt.Fprintf(w,"submit sucess ,temp value : %s",tempValue)
+	}
+	
+
+}
 
 func recieveImage(w http.ResponseWriter , r *http.Request){
 	r.ParseForm()
@@ -27,11 +79,21 @@ func base2Img(imageString string,filename string){
 	err :=ioutil.WriteFile(filename, ddd, 0666) 
 	fmt.Println(err)
 }
+func welcomeUse(w http.ResponseWriter , r *http.Request){
+	r.ParseForm()
+	fmt.Println("path",r.URL.Path)
+	fmt.Fprintf(w,"http server!")
+}
 func main(){
-	fmt.Println(" \t welcome to use recivev image http server \t")
+	fmt.Println(" \t welcome to use  http server \t")
 	fmt.Println("\t\t\t\t\t深圳市德沃仪器\t")
+	http.HandleFunc("/",welcomeUse)
 	http.HandleFunc("/image/upload",recieveImage)
-	err := http.ListenAndServe(":8080",nil)
+	http.HandleFunc("/standard_value/update",updateTempValue)
+	http.HandleFunc("/standard_value/low",updateTempValue)
+	http.HandleFunc("/standard_value/mediume",updateTempValue)
+	http.HandleFunc("/standard_value/high",updateTempValue)
+	err := http.ListenAndServe(":8000",nil)
 	if err != nil{
 		log.Fatal("ListenAndServe:",err)
 	}
